2e4ed8dd22471913dfe7604de712d966371cba9c
3ll3n <ellengrafton01@gmail.com>
2017-03-03 21:50:29 +0000

    Added tests to get/set age to Tiger. Setup ClownFish constructor

--- app/src/main/java/com/codeclan/example/zoo/ClownFish.java
+++ app/src/main/java/com/codeclan/example/zoo/ClownFish.java
@@ -1,8 +1,16 @@
-//package com.codeclan.example.zoo;
-//
-///**
-// * Created by user on 03/03/2017.
-// */
-//
-//public class ClownFish extends Animal implements Swimmable {
-//}
+package com.codeclan.example.zoo;
+
+/**
+ * Created by user on 03/03/2017.
+ */
+
+public class ClownFish extends Animal implements Swimmable {
+
+    private String name;
+    private int age;
+
+    public ClownFish(String name, int age) {
+        super(name, age);
+    }
+
+}
--- app/src/test/java/com/codeclan/example/zoo/TestClownFish.java
+++ app/src/test/java/com/codeclan/example/zoo/TestClownFish.java
@@ -1,8 +1,21 @@
 package com.codeclan.example.zoo;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by user on 03/03/2017.
  */
 
 public class TestClownFish {
+
+    ClownFish clownfish;
+
+    @Before
+    public void before() {
+        clownfish = new ClownFish("Nemo", 2);
+    }
+    
 }
