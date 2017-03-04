858cb77d43f5cab034d60aa124b073fbf865970f
3ll3n <ellengrafton01@gmail.com>
2017-03-04 16:36:29 +0000

    Added fly test/method to Parrot

--- app/src/main/java/com/codeclan/example/zoo/Parrot.java
+++ app/src/main/java/com/codeclan/example/zoo/Parrot.java
@@ -1,8 +1,19 @@
-//package com.codeclan.example.zoo;
-//
-///**
-// * Created by user on 03/03/2017.
-// */
-//
-//public class Parrot extends Animal implements Flyable {
-//}
+package com.codeclan.example.zoo;
+
+/**
+ * Created by user on 03/03/2017.
+ */
+
+public class Parrot extends Animal implements Flyable {
+
+    private String name;
+    private int age;
+
+    public Parrot(String name, int age) {
+        super(name, age);
+    }
+
+    public String fly() {
+        return "I can fly!";
+    }
+}
--- app/src/test/java/com/codeclan/example/zoo/TestParrot.java
+++ app/src/test/java/com/codeclan/example/zoo/TestParrot.java
@@ -1,8 +1,25 @@
 package com.codeclan.example.zoo;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by user on 03/03/2017.
  */
 
 public class TestParrot {
+
+    Parrot parrot;
+
+    @Before
+    public void before() {
+        parrot = new Parrot("Polymorphism", 20);
+    }
+
+    @Test
+    public void testFly() {
+        assertEquals("I can fly!", parrot.fly());
+    }
 }
