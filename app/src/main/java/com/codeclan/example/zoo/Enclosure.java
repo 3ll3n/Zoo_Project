88c6b691c2ff9db042f8da8a5f1ae3f996288e51
3ll3n <ellengrafton01@gmail.com>
2017-03-04 21:19:38 +0000

    Added test/methods to Enclosure to addAnimaland getNumberOfAnimals

--- app/src/main/java/com/codeclan/example/zoo/Enclosure.java
+++ app/src/main/java/com/codeclan/example/zoo/Enclosure.java
@@ -1,8 +1,28 @@
 package com.codeclan.example.zoo;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Created by user on 03/03/2017.
  */
 
 public class Enclosure {
+
+    private String name;
+    private ArrayList<Animal> animals;
+
+
+    public Enclosure(String name) {
+        this.name = name;
+        this.animals = new ArrayList<Animal>();
+    }
+
+    public void addAnimal(Animal animal) {
+       this.animals.add(animal);
+    }
+
+    public int getNumberOfAnimals() {
+        return animals.size();
+    }
 }
--- app/src/test/java/com/codeclan/example/zoo/TestEnclosure.java
+++ app/src/test/java/com/codeclan/example/zoo/TestEnclosure.java
@@ -1,8 +1,49 @@
 package com.codeclan.example.zoo;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by user on 03/03/2017.
  */
 
 public class TestEnclosure {
+
+    Enclosure enclosure;
+    Tiger tiger;
+    Parrot parrot;
+
+
+    @Before
+    public void before() {
+        enclosure = new Enclosure("Tigers");
+        tiger = new Tiger("Tony", 8);
+        parrot = new Parrot("Polymorphism", 20);
+    }
+
+    @Test
+    public void testCanAddAnimal() {
+        enclosure.addAnimal(tiger);
+        assertEquals(1, enclosure.getNumberOfAnimals());
+    }
+
+   @Test
+    public void testNumberOfAnimals() {
+       enclosure.addAnimal(tiger);
+       enclosure.addAnimal(parrot);
+       assertEquals(2, enclosure.getNumberOfAnimals());
+   }
+//
+//    @Test
+//    public void testCanFeedAnimal() {
+//
+//    }
+
+
+
+
 }
