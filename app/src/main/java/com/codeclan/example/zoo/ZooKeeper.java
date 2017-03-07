489bf0fb1e50b739111f053bf27c1b47be046a8b
3ll3n <ellengrafton01@gmail.com>
2017-03-07 11:21:18 +0000

    Updated ZooKeeper

--- app/src/main/java/com/codeclan/example/zoo/ZooKeeper.java
+++ app/src/main/java/com/codeclan/example/zoo/ZooKeeper.java
@@ -10,18 +10,32 @@ public class ZooKeeper {
     public static void main(String[] args) {
 
         Zoo zoo = new Zoo("Animal Sanctuary");
+        Enclosure enclosure = new BigCat("Big Cats");
+        Enclosure enclosure1 = new Aquarium("Aquarium");
+        Enclosure enclosure2 = new Aviary("Aviary");
 
-        System.out.println("Type 1 to add a new enclosure");
-        System.out.println("Type 2 to add an animal to an enclosure");
-        System.out.println("Type 3 to feed an animal");
+        System.out.println("Type 1 to add a new enclosure.");
+        System.out.println("Type 2 to add an animal to an enclosure.");
+        System.out.println("Type 3 to feed an animal.");
 
         Scanner sc = new Scanner(System.in);
 
         if (sc.nextInt() == 1) {
-            Enclosure enclosure = new BigCat("Big Cats");
+            System.out.println("The enclosures you can choose from are: Big Cats, Aquarium or Aviary");
+            if (sc.nextLine() == "Big Cats");
             zoo.addEnclosure(enclosure);
-            System.out.println("Big Cats enclosure added!");
+            System.out.println("..... enclosure added!");
         }
+
+        if (sc.nextInt() == 2) {
+            System.out.println("The animals you can choose to add are: Tiger, ClownFish or Parrot");
+        }
+
+        if (sc.nextInt() == 3) {
+            System.out.println("The animals you can feed are: Tiger, ClownFish or Parrot");
+        }
+
+
 
 
     }
