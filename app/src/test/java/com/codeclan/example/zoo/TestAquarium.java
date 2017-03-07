192dcec5daddca62d03f8a530a2f95089f36f69d
3ll3n <ellengrafton01@gmail.com>
2017-03-07 09:53:01 +0000

    Added testCantAddWrongAnimal to Aquarium

--- app/src/test/java/com/codeclan/example/zoo/TestAquarium.java
+++ app/src/test/java/com/codeclan/example/zoo/TestAquarium.java
@@ -13,17 +13,25 @@ public class TestAquarium {
 
     Enclosure aquarium;
     Animal clownfish;
+    Animal tiger;
 
     @Before
     public void before() {
         aquarium = new Aquarium("Aquarium");
         clownfish = new ClownFish("Flounder", 7);
+        tiger = new Tiger("Spartan", 5);
     }
 
     @Test
     public void testAddAnimal() {
         aquarium.addAnimal(clownfish);
         assertEquals(1, aquarium.getNumberOfAnimals());
+    }
+
+    @Test
+    public void testCantAddWrongAnimal() {
+        aquarium.addAnimal(tiger);
+        assertEquals(0, aquarium.getNumberOfAnimals());
     }
 
 }
