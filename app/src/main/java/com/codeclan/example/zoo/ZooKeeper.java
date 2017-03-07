969fbc2a9dbefa15cecaf72f0d5db033864b772b
3ll3n <ellengrafton01@gmail.com>
2017-03-07 11:06:53 +0000

    Started ZooKeeper runner

--- app/src/main/java/com/codeclan/example/zoo/ZooKeeper.java
+++ app/src/main/java/com/codeclan/example/zoo/ZooKeeper.java
@@ -1,11 +1,28 @@
 package com.codeclan.example.zoo;
 
+import java.util.Scanner;
+
 /**
  * Created by user on 03/03/2017.
  */
 
 public class ZooKeeper {
     public static void main(String[] args) {
+
+        Zoo zoo = new Zoo("Animal Sanctuary");
+
+        System.out.println("Type 1 to add a new enclosure");
+        System.out.println("Type 2 to add an animal to an enclosure");
+        System.out.println("Type 3 to feed an animal");
+
+        Scanner sc = new Scanner(System.in);
+
+        if (sc.nextInt() == 1) {
+            Enclosure enclosure = new BigCat("Big Cats");
+            zoo.addEnclosure(enclosure);
+            System.out.println("Big Cats enclosure added!");
+        }
+
 
     }
 }
